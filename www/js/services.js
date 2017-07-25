b9f53364d2b997b30ff471be0c3f1d5d6f6afd6c
406798106@qq.com <406798106@qq.com>
2017-07-25 10:53:52 +0800

    博绿固废回收APP

--- www/js/services.js
+++ www/js/services.js
@@ -1573,7 +1573,7 @@ angular.module('starter.services', [])
       }
     }
   })
-  .service('WeiXinService', function ($q, $http, BoRecycle, AccountService) { //微信 JS SDK 接口服务定义
+  .service('WeiXinService', function ($q, $http, BoRecycle, AccountService, $state) { //微信 JS SDK 接口服务定义
     return {
       //获取微信access_token
       getWCtoken: function () {
@@ -1752,8 +1752,11 @@ angular.module('starter.services', [])
               "paySign": data.sign //微信签名
             },
             function (res) {
-              alert(JSON.stringify(res));
-              console.log(res.err_msg);  // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返
+              if (res.err_msg == "get_brand_wcpay_request:ok") { // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                $state.go("wallet");
+              }
+              /*   alert(JSON.stringify(res));*/
+              console.log(res);  // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返
             }
           );
         }
