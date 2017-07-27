479bc3af71cb9e77c3e4c912e8e403e732f78fab
406798106@qq.com <406798106@qq.com>
2017-07-27 18:34:42 +0800

    博绿固废回收APP

--- www/js/services.js
+++ www/js/services.js
@@ -2692,7 +2692,7 @@ angular.module('starter.services', [])
         var promise = deferred.promise;
         promise = $http({
           method: 'GET',
-          url: './js/bank.json',
+          url: 'templates/data/bank.json',
         }).success(function (data) {
           deferred.resolve(data);// 声明执行成功，即http请求数据成功，可以返回数据了
         }).error(function (err) {
