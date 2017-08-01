8b1fc70e009fa979b9ce54a8ec444b16bb83539a
406798106@qq.com <406798106@qq.com>
2017-08-01 21:06:02 +0800

    收收APP

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -780,6 +780,7 @@ angular.module('starter.controllers', [])
         })
       }
     }
+
 //获取产品品类
     OrderService.getProductList({ID: "", Name: ""}).success(function (data) {
       console.log(data);
@@ -951,10 +952,13 @@ angular.module('starter.controllers', [])
                 localStorage.setItem("usertype", (services == null || services.length == 0) ? 0 : (services.length == 1 && services.indexOf('1') != -1) ? 1 : 2);
               }
             }).then(function () {
-              var user = JSON.parse(localStorage.getItem("user"));
-              if (user.certstate.substr(3, 1) != 2) { //没有实名认证
-                CommonService.showConfirm('收收提示', '尊敬的用户,您好！实名认证完善认证信息后才能进行更多操作！', '实名认证', '暂不认证', 'realname', 'close', '', {status: 0});
-                return;
+              if ($scope.user.usertype == 2) {
+                CommonService.platformPrompt("完善资料提交成功", 'close');
+                var user = JSON.parse(localStorage.getItem("user"));
+                if (user.certstate.substr(3, 1) != 2) { //没有实名认证
+                  CommonService.showConfirm('收收提示', '尊敬的用户,您好！实名认证完善认证信息后才能进行更多操作！', '实名认证', '暂不认证', 'realname', 'close', '', {status: 0});
+                  return;
+                }
               }
               CommonService.platformPrompt("完善资料提交成功", '');
             });
