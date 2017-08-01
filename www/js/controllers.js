e087f251b351752826d64ee27edc20c11c945c64
406798106@qq.com <406798106@qq.com>
2017-08-01 17:26:31 +0800

    收收APP

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -364,6 +364,7 @@ angular.module('starter.controllers', [])
     localStorage.removeItem("user");
     localStorage.removeItem("usertype");
     localStorage.removeItem("openid");
+    localStorage.removeItem("token");
 
     //如果没有授权先授权 或者超过两个小时
     if (!localStorage.getItem("token") || ((new Date().getTime() - new Date(localStorage.getItem("expires_in")).getTime()) / 1000) > 7199) {
@@ -463,6 +464,7 @@ angular.module('starter.controllers', [])
     localStorage.removeItem("user");
     localStorage.removeItem("usertype");
     localStorage.removeItem("openid");
+    localStorage.removeItem("token");
 
     //如果没有授权先授权 或者超过两个小时
     if (!localStorage.getItem("token") || ((new Date().getTime() - new Date(localStorage.getItem("expires_in")).getTime()) / 1000) > 7199) {
