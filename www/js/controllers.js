676978d100f11647637679324776769ad488f044
visiter2006 <visiter_2006@163.com>
2017-08-16 17:33:45 +0800

    提现增加实名认证判断

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3572,6 +3572,29 @@ angular.module('starter.controllers', [])
     MyWalletService.bankget_count(localStorage.getItem("userid")).success(function (data) {
       $scope.total = data.data;
     });
+    //根据会员ID获取会员账号基本信息
+    if(!localStorage.getItem("user")) {
+      AccountService.getUser({userid: localStorage.getItem("userid")}).success(function (data) {
+        if (data.code == 1001) {
+          $rootScope.userdata = data.data;
+          localStorage.setItem("user", JSON.stringify(data.data));
+        } else {
+          CommonService.platformPrompt(data.message, 'close');
+        }
+      });
+    }
+    $scope.cash=function () {
+      var userCertState=JSON.parse(localStorage.getItem("user")).certstate.split('');
+      console.log(userCertState);
+      if(userCertState[3]!=2){
+        CommonService.showConfirm('收收提示', '尊敬的用户,您好！为了您的账户安全，请先进行实名认证！', '实名认证', '暂不认证', 'tworealname', 'close', '', {status: 0});
+        return;
+      }else {
+        $state.go('cash');
+        return;
+      }
+
+    }
   })
 
   //提现
